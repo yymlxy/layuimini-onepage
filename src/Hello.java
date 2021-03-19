4ee92a5a21e3107466abbaab3d0031cf6806ad63
yymlxy <358285597@qq.com>
2021-03-19 09:17:08 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "hello",urlPatterns = "/hellotest")
+public class Hello extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("国产软件自主可控");
+
+
+    }
+}
